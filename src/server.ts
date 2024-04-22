01ef9efafc13e6796e9364759df4fff505cd8392
Matheussmaced <mateusbyakuya3@gmail.com>
2024-04-22 19:49:54 -0300

    feat: creating server

---
+++ src/server.ts
@@ -0,0 +1,9 @@
+import fastify, { FastifyInstance } from 'fastify';
+
+const app: FastifyInstance = fastify({logger: true})
+
+app.listen({
+  port: 3333
+},
+() => console.log('Server is running on port 3333')
+)
